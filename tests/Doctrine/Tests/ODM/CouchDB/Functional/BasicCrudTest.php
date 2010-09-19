f0eae6a2cefcb18eda80c714c57146f423312610
beberlei <kontakt@beberlei.de>
2010-09-19 14:35:47 +0200

    Add test-case trying multiple insert/updates in one flush operation

--- tests/Doctrine/Tests/ODM/CouchDB/Functional/BasicCrudTest.php
+++ tests/Doctrine/Tests/ODM/CouchDB/Functional/BasicCrudTest.php
@@ -85,6 +85,33 @@ class BasicCrudTest extends \Doctrine\Tests\ODM\CouchDB\CouchDBFunctionalTestCas
         $this->setExpectedException('Doctrine\ODM\CouchDB\DocumentNotFoundException');
         $newUser = $this->dm->find(1);
     }
+
+    public function testInsertUpdateMultiple()
+    {
+        $user1 = $this->dm->find(1);
+        $user1->username = "new-name";
+
+        $user2 = new User();
+        $user2->id = "myuser-1111";
+        $user2->username = "test";
+
+        $user3 = new User();
+        $user3->id = "myuser-2222";
+        $user3->username = "test";
+
+        $this->dm->persist($user2);
+        $this->dm->persist($user3);
+        $this->dm->flush();
+        $this->dm->clear();
+
+        $pUser1 = $this->dm->find(1);
+        $pUser2 = $this->dm->find('myuser-1111');
+        $pUser3 = $this->dm->find('myuser-2222');
+
+        $this->assertEquals('new-name', $pUser1->username);
+        $this->assertEquals('myuser-1111', $pUser2->id);
+        $this->assertEquals('myuser-2222', $pUser3->id);
+    }
 }
 
 class User
