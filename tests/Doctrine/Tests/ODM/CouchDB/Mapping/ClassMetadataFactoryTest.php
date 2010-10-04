7a7bac5ba7e1aaece38f87ee560ccec7695555fa
Lukas Kahwe Smith <smith@pooteeweet.org>
2010-10-04 08:35:47 +0200

    added test for getAllMetadata()

--- tests/Doctrine/Tests/ODM/CouchDB/Mapping/ClassMetadataFactoryTest.php
+++ tests/Doctrine/Tests/ODM/CouchDB/Mapping/ClassMetadataFactoryTest.php
@@ -6,10 +6,14 @@ use Doctrine\ODM\CouchDB\Mapping\ClassMetadataFactory;
 
 class ClassMetadataFactoryTest extends \PHPUnit_Framework_Testcase
 {
+    public function setUp()
+    {
+        $this->dm = \Doctrine\ODM\CouchDB\DocumentManager::create();
+    }
+
     public function testNotMappedThrowsException()
     {
-        $dm = \Doctrine\ODM\CouchDB\DocumentManager::create();
-        $cmf = new ClassMetadataFactory($dm);
+        $cmf = new ClassMetadataFactory($this->dm);
 
         $this->setExpectedException('Doctrine\ODM\CouchDB\Mapping\MappingException');
         $cmf->getMetadataFor('unknown');
@@ -17,13 +21,28 @@ class ClassMetadataFactoryTest extends \PHPUnit_Framework_Testcase
 
     public function testGetMapping()
     {
-        $dm = \Doctrine\ODM\CouchDB\DocumentManager::create();
+        $cmf = new ClassMetadataFactory($this->dm);
+
         $cm = new \Doctrine\ODM\CouchDB\Mapping\ClassMetadata('stdClass');
 
-        $cmf = new ClassMetadataFactory($dm);
         $cmf->setMetadataFor('stdClass', $cm);
 
         $this->assertTrue($cmf->hasMetadataFor('stdClass'));
         $this->assertSame($cm, $cmf->getMetadataFor('stdClass'));
+    }
+
+    public function testGetAllMetadata()
+    {
+        $driver = new \Doctrine\ODM\CouchDB\Mapping\Driver\PHPDriver(array(__DIR__));
+        $this->dm->getConfiguration()->setMetadataDriverImpl($driver);
+
+        $cmf = new ClassMetadataFactory($this->dm);
+
+        $cm = new \Doctrine\ODM\CouchDB\Mapping\ClassMetadata('stdClass');
+        $cmf->setMetadataFor('stdClass', $cm);
+
+        $metadata = $cmf->getAllMetadata();
+
+        $this->assertType('array', $metadata);
     }
 }
