292e218989358033e80a62beba5a9658a5abc49a
Benjamin Eberlei <kontakt@beberlei.de>
2010-11-28 23:41:11 +0100

    Fix FolderDesignDocument to search for .js not .json files (stupid).

--- lib/Doctrine/ODM/CouchDB/View/FolderDesignDocument.php
+++ lib/Doctrine/ODM/CouchDB/View/FolderDesignDocument.php
@@ -27,8 +27,8 @@ class FolderDesignDocument implements DesignDocument
 
             $this->data = array();
             foreach ($ri AS $path) {
-                if (substr($path, -5) === ".json") {
-                    $parts = explode("/", ltrim(str_replace($this->folderPath, '', str_replace(".json", "", $path)), '/'));
+                if (substr($path, -3) === ".js") {
+                    $parts = explode("/", ltrim(str_replace($this->folderPath, '', str_replace(".js", "", $path)), '/'));
 
                     if (count($parts) == 3) {
                         $this->data[$parts[0]][$parts[1]][$parts[2]] = file_get_contents($path);
