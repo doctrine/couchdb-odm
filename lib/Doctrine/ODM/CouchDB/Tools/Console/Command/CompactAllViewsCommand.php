ee76741d33faa7958eae12f2eaa56be96ec43b28
Christian Tischer <cht@crosscan.com>
2016-12-20 13:51:24 +0100

    Added command to compact all couchDB views

---
+++ lib/Doctrine/ODM/CouchDB/Tools/Console/Command/CompactAllViewsCommand.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\ODM\CouchDB\Tools\Console\Command;
+
+use Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface,
+    Symfony\Component\Console\Command\Command;
+
+class CompactAllViewsCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('couchdb:odm:compact-all-views')
+            ->setDescription('Compact all views.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dm = $this->getHelper('couchdb')->getDocumentManager();
+        $couchDbClient = $dm->getCouchDBClient();
+        $config = $dm->getConfiguration();
+
+        $designDocNames = $config->getDesignDocumentNames();
+
+        foreach ($designDocNames as $docName) {
+            $designDocData = $config->getDesignDocument($docName);
+
+            $localDesignDoc = new $designDocData['className']($designDocData['options']);
+            $localDocBody = $localDesignDoc->getData();
+
+            if (array_key_exists('views', $localDocBody)) {
+                $couchDbClient->compactView($docName);
+                $output->writeln(sprintf(
+                    'Started compaction for view %s.',
+                    $docName
+                ));
+            }
+        }
+    }
+}
--- sandbox/doctrine.php
+++ sandbox/doctrine.php
@@ -12,5 +12,6 @@ $cli->addCommands(array(
     new \Doctrine\CouchDB\Tools\Console\Command\CompactViewCommand(),
     new \Doctrine\CouchDB\Tools\Console\Command\MigrationCommand(),
     new \Doctrine\ODM\CouchDB\Tools\Console\Command\UpdateDesignDocCommand(),
+    new \Doctrine\ODM\CouchDB\Tools\Console\Command\CompactAllViewsCommand(),
 ));
 $cli->run();
